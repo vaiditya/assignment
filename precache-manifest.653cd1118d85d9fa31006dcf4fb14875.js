eccdb881cf1828240d4ddfa2111d1dd499b1117c
Vaiditya <vaidityachauhan@gmail.com>
2020-03-23 09:09:16 +0530

    Create gh-pages branch with static content 1

---
+++ precache-manifest.653cd1118d85d9fa31006dcf4fb14875.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28def1ffee02ff1feeb60607f91eacc9",
+    "url": "/assignment/index.html"
+  },
+  {
+    "revision": "0d0910628f1d26fce62a",
+    "url": "/assignment/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "25b81e79d4f6705fd581",
+    "url": "/assignment/static/js/2.146a2d59.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/assignment/static/js/2.146a2d59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d0910628f1d26fce62a",
+    "url": "/assignment/static/js/main.c829d66b.chunk.js"
+  },
+  {
+    "revision": "0792d1b32ada90080f9b",
+    "url": "/assignment/static/js/runtime-main.e7fee43d.js"
+  }
+]);
