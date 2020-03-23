a932497356780d9f4d5a9b011346696e76af9b2c
Vaiditya <vaidityachauhan@gmail.com>
2020-03-23 09:02:31 +0530

    Create gh-pages branch with static content

--- src/styles.js
+++
@@ -1,5 +0,0 @@
-import styled from "styled-components";
-
-const HomePageStyle = styled.div``;
-
-export default HomePageStyle;
--- src/utils.js
+++
@@ -1,2 +0,0 @@
-export const column = ["Id", "Title", "Status"];
-export const options = ["Done", "Not started", "In Progress"];
